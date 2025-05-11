f57c02e1a25f35b26caba75c6e064efb0a03ab6d
GPIProjeto <projetoingpi@gmail.com>
2025-05-11 18:18:01 -0300

    Create registro_frequencia.h

---
+++ registro_frequencia.h
@@ -0,0 +1,11 @@
+#ifndef REGISTRO_FREQUENCIA_H #define REGISTRO_FREQUENCIA_H
+
+#include "cadastro_aluno.h"
+
+typedef struct { 
+  Aluno aluno; 
+  char data[11]; 
+  int presente;
+} RegistroFrequencia;
+
+void registrar_frequencia(RegistroFrequencia* registro);
